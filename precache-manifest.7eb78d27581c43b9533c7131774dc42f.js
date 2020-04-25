5a26683f006e6750082d6fbac2adc5e4aadc5545
HAuckram <hannnah.auckram@xtra.co.nz>
2020-04-26 01:20:46 +1200

    Updates

---
+++ precache-manifest.7eb78d27581c43b9533c7131774dc42f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db607423c12c5e28c13f98bf29cf649d",
+    "url": "/generate-cafe-ideas/index.html"
+  },
+  {
+    "revision": "6729023c59b91bcc8078",
+    "url": "/generate-cafe-ideas/static/css/main.eca13402.chunk.css"
+  },
+  {
+    "revision": "fcc2ab3e2283422b85c3",
+    "url": "/generate-cafe-ideas/static/js/2.64347571.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/generate-cafe-ideas/static/js/2.64347571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6729023c59b91bcc8078",
+    "url": "/generate-cafe-ideas/static/js/main.aaa0cef0.chunk.js"
+  },
+  {
+    "revision": "dcdebad8e3ae3395599b",
+    "url": "/generate-cafe-ideas/static/js/runtime-main.f5a836d4.js"
+  }
+]);
