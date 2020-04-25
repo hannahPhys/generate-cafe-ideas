6d3128dde6317f5995def0322891e737d0f51117
HAuckram <hannnah.auckram@xtra.co.nz>
2020-04-26 01:03:03 +1200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Toggle from './Components/testButton.js';
+
+import './Styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <p>
+        Cafe Idea Generator
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Toggle />
     </div>
   );
 }
 
 export default App;
+
+
