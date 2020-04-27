c159f3dabc1094a03cf09a27a5f5048b8e94c6f6
HAuckram <hannnah.auckram@xtra.co.nz>
2020-04-27 17:52:45 +1200

    change color on click

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import GeneratorButton from './Components/generateButton.js';
 import Footer from './Components/footer.js';
 
 import './sass/App.scss';
 
-function App() {
-  return (
-    <div className="App ">
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bgColor: ""
+    }
+  }
+
+  boxClick = (e) => {
+    var colors = ["#2d3e37",
+      "#7c7563",
+      "#a7947a",
+      "#d1bfa5",
+      "#e1ccai",
+      "#b3ac91",
+      "#ccc5b8",
+      "#cdc4ad",
+      "#d3c0a4",
+      "#b8b78a",
+      "#ece6d0",
+      "#ddd3c4",
+      "#ddd1b5",
+      "#dad1ba"]
+    var result = this.randomValue(colors);
+
+    this.setState({
+      bgColor: result
+    })
+  }
+
+  randomValue(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+  render() {
+    return (<div className="App" style={{ backgroundColor: this.state.bgColor }} >
       <div className="HeaderTopLeft">
         <h1>
           Cafe Idea Generator
         </h1>
       </div>
-      <GeneratorButton />
+      <GeneratorButton parentMethod={this.boxClick} />
       <><Footer /></>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/Components/generateButton.js
+++ src/Components/generateButton.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import App from '../App.js';
 
 class GeneratorButton extends React.Component {
 	constructor(props) {
@@ -8,9 +9,11 @@ class GeneratorButton extends React.Component {
 	}
 
 	handleClick() {
-		var adjectives = ["Modern", "Boho", "Simple", "Quiet", "Quirky", "Rustic", "Classic", "90's", "80's", "70's", "2000's", "Retro", "Chic", "Spacey", "Urban", "Rural", "Oaky", "Surfy", "Carnival", "Ugly", "Vegan", "Apocalyptic", "Family-Friendly", "Healthy", "Hipster", "Royal", "Greek", "Hippie", "Arabian", "Western", "Outdoor", "Caribbean", "Futuristic", "Parisian", "Co-working", "Student", "Eco-Friendly", "Expenisve", "High Class", "5 Star", "Budget"];
+		this.props.parentMethod();
 
-		var nouns = ["Safari", "Beach", "Garden", "Neon", "Rooftop", "Cat", "Boatshed", "Country", "Oktoberfest", "Chocolate", "Disney", "Health", "Vegan", "Family-Friendly", "Children's", "Playground", "Disco", "Turkish", "Pirate", "Star Wars", "Apres Ski", "BBQ", "Festival", "Student", "Co-working", "Sidewalk", "Takeaway", "Fitness", "Juice", "Backpacker"];
+		var adjectives = ["Modern", "Boho", "Simple", "Quiet", "Quirky", "Rustic", "Classic", "90's", "80's", "70's", "2000's", "Retro", "Chic", "Spacey", "Urban", "Rural", "Oaky", "Surfy", "Carnival", "Ugly", "Vegan", "Apocalyptic", "Family-Friendly", "Healthy", "Hipster", "Royal", "Greek", "Hippie", "Arabian", "Western", "Outdoor", "Caribbean", "Futuristic", "Parisian", "Co-working", "Student", "Eco-Friendly", "Expensive", "High Class", "5 Star", "Budget"];
+
+		var nouns = ["Safari", "Beach", "Garden", "Neon", "Rooftop", "Cat", "Boatshed", "Country", "Oktoberfest", "Chocolate", "Disney", "Health", "Vegan", "Family-Friendly", "Children's", "Playground", "Disco", "Turkish", "Pirate", "Star-Wars", "Apres Ski", "BBQ", "Festival", "Student", "Co-working", "Sidewalk", "Takeaway", "Fitness", "Juice", "Backpacker"];
 
 		var cafeTypes = ["Bakery", "Restaurant", "Shop", "Cafeteria", "Buffet", "Tea Rooms", "Tea Shop", "Bistro", "Wine Bar", "Bar", "Pub", "Eatery", "Caf", "Coffee House", "Tavern"];
 
